8d1caadd85b64d93b0d0452e438ceff8ea9feb86
Megan McBride <meganambermcbride@gmail.com>
2022-09-10 15:56:45 -0400

    Install react router and packages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 
 function App() {
   return (
+    <Router >
     <div>
+      <Route path=''>
       <Navigation />
+
+      </Route>
+      <Route path='/:type'>
+
       <HomePage />
+      </Route>
     </div>
+
+    </Router>
   );
 }
 
