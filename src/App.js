fa3ed7211762a55ef7479e8c57f50e504abe3436
Megan McBride <meganambermcbride@gmail.com>
2022-09-10 20:16:49 -0400

    Add useParams to get individual pet page and switch

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <Router >
-    <div>
-      <Route path=''>
-      <Navigation />
-
-      </Route>
-      <Route path='/:type?'>
-
-      <HomePage />
-      </Route>
-    </div>
-
+    <Router>
+        <main>
+          <Route path="/">
+            <Navigation />
+          </Route>
+      <Switch>
+          <Route path="/:type/:id">
+            <PetDetailsPage />
+          </Route>
+          <Route path="/:type?">
+            <HomePage />
+          </Route>
+      </Switch>
+        </main>
     </Router>
   );
 }
